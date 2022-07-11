d8ed331f8a2832aec660b63fd2ac22b4610f2e53
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-11 17:31:22 +0300

    Testing out collaboration

--- main.h
+++ main.h
@@ -5,11 +5,6 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-<<<<<<< HEAD
 char *prep_string(char *str, specifier spec);
-=======
-int bavin(int n);
-
->>>>>>> 2a0980c66bfb87b41e96c1ae0d6e9afd13ef14e7
 
 #endif
