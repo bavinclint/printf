3c83a03735b8356baf72faf0f77fba716712dfe8
Muriuki Wanjohi <81047807+Vashow1@users.noreply.github.com>
2022-07-11 16:30:20 +0300

    Updating the char switch case

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # printf
+
+//auto_push_git
+Is a bash file that automates the pushing of commits though each times you must change the 'updates' commit message so as to document why the commit message.
+
+// protoprintf.h //
+This header file contains the declarations of the functions to be used in the printf function.
---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "protoprintf.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * 
+ * format: is a character string. The format string is composed of
+ * zero of more directives
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{}
