0cfb0628a34f83c5a7f9f949f421700bf1018f89
bavinclint <bavinclint@gmial.com>
2022-07-11 08:02:16 -0700

    main header

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
-#ifndef PROTOPRINTF_H
-#define PROTOPRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-char *prep_string(char *str, specifier spec);
+
 
 #endif
