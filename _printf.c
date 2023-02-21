8c7d50c21c086f1c5ad3e11a657af6d3d375353d
bavinclint <bavinclint@gmail.com>
2023-02-22 01:01:07 +0300

    updated file

--- _printf.c
+++ _printf.c
@@ -1,68 +1,86 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _printf - a function that produces output according to a format.
- * 
- * format: is a character string. The format string is composed of
- * zero of more directives
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+
+int (*get_op(const char c))(va_list)
+{
+	int i = 0;
+
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
  */
 
 int _printf(const char *format, ...)
 {
-	int len = _strlen(format);
-	const int initialLen = len;
-	int index = 0, uindex = 0;
-	char *updatedformat = malloc(sizeof(char) * len);
-	char *newformat, *anotherformat;
 	va_list ap;
+	int sum = 0, i = 0;
+	int (*func)();
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(ap, format);
 
-	while (index <= initialLen)
+	while (format[i])
 	{
-		if (format[index] == '%')
+		if (format[i] == '%')
 		{
-			if (format[index + 1] == 'c')
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
+			if (func == NULL)
 			{
-				char character = va_arg(ap, int);
-				updatedformat[uindex] = character;
-				len -= 1;
-				index += 2;
-				uindex++;
+				_putchar(format[i]);
+				sum++;
+				i++;
 			}
-			else if (format[index + 1] == 's')
+			else
 			{
-				const char *myString = va_arg(ap, const char*);
-				int sLen = _strlen(myString);
-				len += sLen - 2;
-				newformat = realloc(updatedformat, sizeof(char) * len);
-				updatedformat = newformat;
-				updatedformat = _strncat(updatedformat, myString, uindex);
-				index += 2;
-				uindex += sLen;
+				sum += func(ap);
+				i += 2;
+				continue;
 			}
-			else if(format[index + 1] == '%')
-			{
-				updatedformat[uindex] = '%';
-				len -= 1;
-				index += 2;
-				uindex++;
-			}
-		 } else
-		 {
-			updatedformat[uindex] = format[index];
-			index++;
-			uindex++;
-		 }
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+			i++;
+		}
 	}
-
-	anotherformat = realloc(updatedformat, sizeof(char) * len);
-	updatedformat = anotherformat;
-	_puts(updatedformat);
-	free(updatedformat);
-	return (len);
+	va_end(ap);
+	return (sum);
 }
